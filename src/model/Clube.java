67afcaca6e618fa75ce88caa6c0cd83ca8cb519e
gabsKlaudio <gabrielly.claudio@sou.unaerp.edu.br>
2026-03-29 15:09:40 -0300

    Adicionando a classe clube

---
+++ src/model/Clube.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Clube {
+    private String nome;
+
+    public Clube(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}
